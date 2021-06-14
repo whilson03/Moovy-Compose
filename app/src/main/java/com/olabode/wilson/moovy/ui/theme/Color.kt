75205618c85c5f678286b1b838db48617e83b072
wilson <whilson03@gmail.com>
2021-06-14 18:55:27 +0100

    change color

--- app/src/main/java/com/olabode/wilson/moovy/ui/theme/Color.kt
+++ app/src/main/java/com/olabode/wilson/moovy/ui/theme/Color.kt
@@ -2,11 +2,6 @@ package com.olabode.wilson.moovy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFF2C3848)
-val Purple500 = Color(0xFF5CA0D3)
-val Purple700 = Color(0xFF5CA0D3)
-val Teal200 = Color(0xFF5CA0D3)
-
 val deepBlue = Color(0xFF2C3848)
 val lightBlue = Color(0xFF5CA0D3)
 
--- app/src/main/java/com/olabode/wilson/moovy/ui/theme/Theme.kt
+++ app/src/main/java/com/olabode/wilson/moovy/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = lightBlue,
+    primaryVariant = deepBlue,
+    secondary = deepBlue
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = lightBlue,
+    primaryVariant = deepBlue,
+    secondary = deepBlue
 
     /* Other default colors to override
     background = Color.White,
